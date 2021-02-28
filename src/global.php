e43542c31884895f85ca6b25db317b91b494a614
Ankit Pal <55882539+ankit9889@users.noreply.github.com>
2021-03-01 00:16:25 +0530

    Update global.php

--- src/global.php
+++ src/global.php
@@ -1,14 +1,18 @@
 <?php
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 if (! function_exists('GetMenuByName')) {
 function GetMenuByName($name)
 {
-    $file = $name;
-    $destinationPath=storage_path('app/upload/menu_json/').'/';
-    $getMenu = file_get_contents(storage_path('app/upload/menu_json/'.$name));
-    $data = json_encode($getMenu, true);
-     $decodedata = json_decode($getMenu);
-    return collect($decodedata);
+    $path ='app/upload/menu_json/'.$name.'.json';
+    if(File::exists(storage_path($path))){
+        $getMenu = file_get_contents(storage_path($path));
+        $data = json_encode($getMenu, true);
+         $decodedata = json_decode($getMenu);
+        return collect($decodedata);
+    };
+    return array('data not found');
 }
 }
 
